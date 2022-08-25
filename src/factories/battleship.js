cdcd3301e9c8be1d74195cc3b69f03edeea1ee1a
Alex <alexandruzmau320@gmail.com>
2022-08-25 15:28:38 +0300

    Create battleship tests / Create battleship factory

--- src/factories/battleship.js
+++ src/factories/battleship.js
@@ -1,5 +1,18 @@
-const battleshipFactory = (length) => {
-  return { length };
+const battleshipFactory = (length, position, orientation) => {
+  const ship = [];
+
+  const buildShip = (() => {
+    let nextPosDistance = orientation === 'horizontal' ? 1 : 10;
+    for (let i = 0; i < length; i++) ship.push(position + i * nextPosDistance);
+  })();
+
+  const hit = (position) => {
+    if (ship[position] !== 'hit') ship[position] = 'hit';
+    return ship;
+  };
+  const isSunk = () => ship.every((pos) => pos === 'hit');
+
+  return { ship, hit, isSunk };
 };
 
 export default battleshipFactory;
--- src/test/battleship.test.js
+++ src/test/battleship.test.js
@@ -1,11 +1,29 @@
 import battleshipFactory from '../factories/battleship';
 
 describe('Battleship', () => {
-  let battleship;
+  let battleshipH, battleshipV;
   beforeEach(() => {
-    battleship = battleshipFactory(3);
+    battleshipH = battleshipFactory(3, 7, 'horizontal');
+    battleshipV = battleshipFactory(2, 4, 'vertical');
   });
+
+  test('was created successfully', () => {
+    expect(battleshipH.ship).toEqual([7, 8, 9]);
+    expect(battleshipV.ship).toEqual([4, 14]);
+  });
+
   test('has correct length', () => {
-    expect(battleship.length).toBe(3);
+    expect(battleshipH.ship.length).toBe(3);
+  });
+
+  test('was hit successfully', () => {
+    let shipAfterHit = battleshipV.hit(0);
+    expect(shipAfterHit).toEqual(['hit', 14]);
+  });
+
+  test('was sunk', () => {
+    battleshipV.hit(0);
+    battleshipV.hit(1);
+    expect(battleshipV.isSunk()).toBe(true);
   });
 });
